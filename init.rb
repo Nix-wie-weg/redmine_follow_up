47b388287892f44bc6f506af03e299e59ccdd912
Sven Winkler <sven.winkler@nix-wie-weg.de>
2012-03-09 19:35:23 +0100

    Better defaults

--- init.rb
+++ init.rb
@@ -1,7 +1,9 @@
+require 'redmine'
+
 Redmine::Plugin.register :follow_up do
   name 'Follow-Up'
   author 'Nix-wie-weg'
   description 'Follow-Ups for Redmine/ChiliProject'
   version '0.0.1'
-  settings :partial => 'settings/settings'
+  settings :default => {'messsage' => '...'}, :partial => 'settings/settings'
 end

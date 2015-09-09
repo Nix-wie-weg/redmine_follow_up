5153eb31a69486637e5d0186d4da0ede3839bbbe
Sven Winkler <svenwin@gmail.com>
2015-09-09 15:58:22 +0200

    #18936: Retrieve all past follow up entries

--- lib/follow_up.rb
+++ lib/follow_up.rb
@@ -1,10 +1,8 @@
 module FollowUp
   def self.process
-    v = CustomValue.find_all_by_custom_field_id_and_value_and_customized_type(
-      settings["custom_field"], Date.today.to_s, "Issue")
-    v.each do |value|
+    query.each do |value|
       issue = value.customized
-      journal = issue.init_journal(user, message)
+      issue.init_journal(user, message)
       value.value = nil
       value.save
       issue.priority = priority
@@ -13,27 +11,35 @@ module FollowUp
     end
   end
 
+  def self.query
+    sql = [
+      'custom_field_id = ?',
+      'customized_type = ?',
+      'DATE(value) < CURDATE()'
+    ].join(' AND ')
+    CustomValue.all(:conditions => [sql, settings['custom_field'], 'Issue'])
+  end
+
   def self.settings
     ::Setting[:plugin_follow_up]
   end
 
   def self.user
-    ::User.find(settings["user"])
+    ::User.find(settings['user'])
   end
 
   def self.priority
-    ::IssuePriority.find(settings["priority"])
+    ::IssuePriority.find(settings['priority'])
   end
 
   def self.message
-    settings["message"].to_s
+    settings['message'].to_s
   end
 
   def self.last_user_for(issue)
     assigned_id = issue.journals.collect do |j|
-      j.changes["assigned_to_id"]
+      j.changes['assigned_to_id']
     end.flatten.compact.last
     User.find(assigned_id || issue.author_id)
   end
-
 end
